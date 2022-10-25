9e4354e834d63ddac70f5b9d59e29f007f19deb4
omerk <omerkarahan1989@gmail.com>
2022-10-25 01:21:19 -0400

    ecommerce part 1 completed

---
+++ src/main/java/com/cydeo/entity/Balance.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Balance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @OneToOne
+    private Customer customer;
+}
